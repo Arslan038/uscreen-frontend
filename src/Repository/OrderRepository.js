83371e3f3600dea96ad939f4e8521a58d77f8d22
VIRGO96 <dawoodahmed696@gmail.com>
2020-05-01 20:20:57 +0500

    order repo

--- src/Repository/OrderRepository.js
+++ src/Repository/OrderRepository.js
@@ -1,12 +1,33 @@
 import Repository from './Repository';
 const order_update_resource = 'order/update';
 const order_list_resource = 'order/list';
+const package_resource = 'package?UserRoleCode=EMPLOYER';
+const package_items_resource = 'package/item';
+const package_items_countries = 'package/item/pricing';
+const new_order = 'order/create';
+const order_charge = 'payment/charge';
 
 export default {
+  getEmployerPackages(){
+    return Repository.get(`${package_resource}`)
+  },
+  
+  getPackageItemCountries(payload) {
+    return Repository.get(`${package_items_countries}?PackageServiceId=${payload}`)
+  },
+  getPackageItems(payload) {
+    return Repository.get(`${package_items_resource}?PackageServiceId=${payload}`)
+  },
   getorders() {
     return Repository.get(`${order_list_resource}`+'?Page=1&Size=5')
   },
-  edit_order(payload) {
-    return Repository.post(`${order_update_resource}`,payload);
-  }
+  order_charge(payload) {
+    return Repository.post(`${order_charge}`,payload);
+  },
+  create_order(payload) {
+    return Repository.post(`${new_order}`,payload);
+  },
+  // edit_order(payload) {
+  //   return Repository.post(`${order_update_resource}`,payload);
+  // }
 }
