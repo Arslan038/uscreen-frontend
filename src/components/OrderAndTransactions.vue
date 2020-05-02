afcd4af4fce9d694267619767f9fb52f56ecdb32
VIRGO96 <dawoodahmed696@gmail.com>
2020-05-02 17:45:58 +0500

    order transactions handled

--- src/components/OrderAndTransactions.vue
+++ src/components/OrderAndTransactions.vue
@@ -15,10 +15,11 @@
                                 <th>Order Status</th>
                                 <th>Report</th>
                                 <th>Invoice</th>
+                                <th>Order Details</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="(item,i) in allorders" :key="i" @click="routeto(item)">
+                            <tr v-for="(item,i) in allorders" :key="i" >
                                 <td>{{item.OrderId}}</td>
                                 <!-- <td>98768731</td> -->
                                 <td>{{item.PackageServiceName}}</td>
@@ -26,42 +27,15 @@
                                 <!-- <td><button class="btn btn-sm btn-border btn-block">in review</button></td> -->
                                 <td>{{item.LastCreated.split('T')[0]}}</td>
                                 <td><button class="btn btn-sm btn-border btn-block">{{getOrderStatus(item.OrderStatusId)}}</button></td>
-                                <td><button class="btn btn-primary btn-sm btn-block">View</button></td>
-                                <td><button class="btn btn-primary btn-sm btn-block">download</button></td>
+                                <td v-if="viewreport[i].isreportshow==false"><button class="btn btn-primary btn-sm btn-block" @click="getViewReport(item.OrderKey,i)">Get</button></td>
+                                <td v-else><a class="btn btn-primary btn-sm btn-block" target="_blank" :href="viewreport[i].reporturl">Click Now</a></td>
+                                <td v-if="viewreport[i].showinvoice==false"><button class="btn btn-primary btn-sm btn-block" @click="getOrderExportReport(item.OrderKey,i)">Get</button></td>
+                                <td v-else><a class="btn btn-primary btn-sm btn-block"  download :href="viewreport[i].invoiceurl">Download Now</a></td>
+
+                                <td><button @click="routeto(item)" class="btn btn-primary btn-sm btn-block">View</button></td>
+
+                                <!-- <td @click="routeto(item)" class="btn btn-primary btn-sm btn-block">View</td> -->
                             </tr>
-                            <!--<tr>
-                                <td>7654378-1</td>
-                                <td>765t654378</td>
-                                <td>Employer - Gold Package</td>
-                                <td>1000</td>
-                                <td><button class="btn btn-sm btn-primary btn-block">ready</button></td>
-                                <td>6-3-2019</td>
-                                <td><button class="btn btn-sm btn-primary btn-block">ready</button></td>
-                                <td><button class="btn btn-primary btn-sm btn-block">View</button></td>
-                                <td><button class="btn btn-primary btn-sm btn-block">download</button></td>
-                             </tr>
-                            <tr>
-                                <td>7654378-1</td>
-                                <td>765t654378</td>
-                                <td>Employer - Gold Package</td>
-                                <td>2400</td>
-                                <td><button class="btn btn-sm btn-primary btn-block">ready</button></td>
-                                <td>6-3-2019</td>
-                                <td><button class="btn btn-sm btn-border btn-block">Pending Payment</button></td>
-                                <td><button class="btn btn-primary btn-sm btn-block">View</button></td>
-                                <td><button class="btn btn-primary btn-sm btn-block">download</button></td>
-                            </tr>
-                            <tr>
-                                <td>7654378-1</td>
-                                <td>765t654378</td>
-                                <td>Employer - Gold Package</td>
-                                <td class="text-warning">(1000)</td>
-                                <td><button class="btn btn-sm btn-border btn-block">cancelled</button></td>
-                                <td>6-3-2019</td>
-                                <td><button class="btn btn-sm btn-border btn-block refunded">Refunded</button></td>
-                                <td><button class="btn btn-primary btn-sm btn-block">View</button></td>
-                                <td><button class="btn btn-primary btn-sm btn-block">download</button></td>
-                            </tr> -->
                         </tbody>
                     </table>
                 </div>
@@ -72,12 +46,58 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import { RepositoryFactory } from '../Repository/RepositoryFactory'
+const OrderRepository = RepositoryFactory.get('order_repository')
+
 export default {
     name: "OrderAndTransactions",
     computed:{
         ...mapGetters(['allorders'])
     },
+    watch:{
+        allorders(){
+            this.setTempViewReports()
+        }
+    },
+    created(){
+        this.fetchOrders()
+        this.setTempViewReports()
+    },
+    data(){
+        return {
+            viewreport:[]
+        }
+    },
     methods:{
+        setTempViewReports(){
+            if(this.allorders.length>0){
+                this.allorders.forEach(item=>{  
+                  this.viewreport.push({isreportshow:false,reporturl:'',invoiceurl:'',showinvoice:false})
+                })
+
+            }
+        },
+        async getViewReport(id,i){
+            // console.log(id)
+            let {data}= await OrderRepository.getOrderReport({OrderKey:id})
+            this.viewreport[i].isreportshow=true
+
+            this.viewreport[i].reporturl=data.data.screening_results_url
+            // console.log(data)
+
+        },
+        async getOrderExportReport(id,i){
+            let {data}=await OrderRepository.getOrderPerforma(id)
+            console.log(data)
+            this.viewreport[i].invoiceurl=data.data[0].File
+            this.viewreport[i].showinvoice=true
+
+        },
+        async fetchOrders(){
+        let {data}= await OrderRepository.getorders()
+        this.$store.commit("setAllOrders",data.data.PageData)
+
+        },
         routeto(item){
             this.$router.push({name:'OrderDetail',params:{selected_order:item}})
 
