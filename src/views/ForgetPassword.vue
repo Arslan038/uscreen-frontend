3178ed295055d9f97912b96d4ca21931bf87e4d6
VIRGO96 <dawoodahmed696@gmail.com>
2020-05-04 14:10:15 +0500

    forget password

---
+++ src/views/ForgetPassword.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="login">
+        <div class="row">
+            <div class="col-12">
+                <Breadcrumb :items="items" class="px-5" />
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="text-center text-head mt-5">Forget Password</h4>
+                    <div class="card my-5">
+                        <div class="card-body pt-3 pb-5 shadow">
+                            <div class="row mx-5">
+                                <div class="col-md-6 offset-md-3 col-12">
+                                    <input type="email" class="form-control mt-3" v-model="forgetObj.Email" placeholder="Email*">
+                                    <input type="password" v-model="forgetObj.Password" class="form-control mt-3" placeholder="Password">
+                                    <!-- <p class="text-primary mt-2">Forget Password</p> -->
+                                </div>
+                                <div class="col-12 text-center">
+                                    <button class="btn btn-primary mt-4" @click="auth_login">Confirm</button>
+                                    <p class="text-head pt-3">Don't have an account yet? <span class="text-primary" @click="$router.push({path: '/signup'})">Sign up</span></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Breadcrumb from '@/components/Breadcrumb.vue'
+import { RepositoryFactory } from '../Repository/RepositoryFactory'
+const UserRepository = RepositoryFactory.get('user_repository')
+import Repository from '../Repository/Repository'
+export default {
+  name: "Login",
+  components: {
+    Breadcrumb
+  },
+  data() {
+    return {
+        forgetObj:{
+            Email:'',
+            Password:''
+        },
+      items: [
+        {
+            text: 'Home',
+            active: false,
+            to: '/'
+        },
+        {
+            text: 'Login',
+            active: true,
+            to: '/login'
+        }
+      ],
+    }
+  },
+  methods:{
+      async auth_login(){
+          let {data} = await UserRepository.forget_password(this.forgetObj)
+          .catch(error => {
+              console.log(error.response)
+              this.$store.commit('setNotifications',{message:error.response.data.Message,type:'error'})
+          });
+            this.$store.commit('setNotifications',{message:'Password reset link sent successfully',type:'success'})
+
+      }
+  }
+}
+</script>
+<style scoped>
+h4 {
+    font-weight: bold;
+}
+.btn-primary {
+    border-radius: 25px;
+    font-weight: bold;
+    padding: 10px 70px;
+}
+.text-primary:hover {
+    cursor: pointer;
+}
+</style>
