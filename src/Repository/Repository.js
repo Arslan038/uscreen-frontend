799887e4db71a2b4db2ca78b8c15a2744aa8340b
Raymond Wong <raymondwong@nexsoftech.com>
2020-05-20 03:21:04 +0800

    Added config.js as config file

--- src/Repository/Repository.js
+++ src/Repository/Repository.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+import config from '../../config.js'
 
-const baseDomain = 'http://128.199.82.172:3000/';
-const baseURL = `${baseDomain}api/`
+const baseURL = config.baseURL;
+// const baseDomain = 'http://128.199.82.172:3000/';
+// const baseURL = `${baseDomain}api/`
 // localStorage.getItem('uscreen-token').replace(/"/g,"")
 export default axios.create({
   baseURL,
